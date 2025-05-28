f3322d6ab3309b8ee6e38a6137a741e988af4329
Diep-LN <d13854002@ntu.edu.tw>
2025-05-28 15:01:56 +0800

    Add a package

--- Dengue assignment 3.R
+++ Dengue assignment 3.R
@@ -1,5 +1,7 @@
 # HRM Assingment 3
-##load csv file
+## Load library
+library(dplyr)
+## Load csv file
 df <- read.csv('/Users/mac/Documents/GitHub/ghp_d13854002/dengue_assignment.csv')
 ## Filter data from 2024 and sum the cases
 dengue_cases_2014 <- sum(df$case_number[df$year == 2014])
